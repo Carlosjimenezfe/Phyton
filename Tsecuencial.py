e155373e2c9e4bc3991cea32d350b483cc2004a1
Carlos Jimenez <valuu1216@gmail.com>
2021-09-03 14:42:57 -0500

    Valor de la masa

--- Tsecuencial.py
+++ Tsecuencial.py
@@ -5,3 +5,12 @@ Created on Fri Sep  3 14:35:31 2021
 @author: Usuario
 """
 
+"""
+1. Haga un algoritmo que calcule la masa de la siguiente fórmula:
+Masa = (presión * volúmen) / (0.37 * (temperatura + 460))
+"""
+presion = float(input('Digite el valor de la presion : '))
+volumen = float(input('Digite el valor del volumen :'))
+temperatura = float(input('Digite el valor de la temperatura : '))
+masa = (presion * volumen ) / (0.37 * ( temperatura + 460 ) )
+print(f'El valor de la masa es de : {masa} ' )
